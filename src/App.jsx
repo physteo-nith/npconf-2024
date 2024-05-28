15190435380b3e9d6a618d00b323b17afe8bdaa6
shabd patel <shabdpatel0@gmail.com>
2024-05-28 15:01:43 +0530

    created faq page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
+import FAQ from './components/faq/Faq'
 
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
       <section id='Speakers'>Speakers</section>
       <section id='Sponsors'>Sponsors</section>
       <section id='Team'>Team</section>
-      <section id='FAQ'>FAQ</section>
+      <section id='FAQ'><FAQ /></section>
     </div>
   )
 }
